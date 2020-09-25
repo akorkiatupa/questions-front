c9d395086b87867b08cece1748daaadb1305cb1d
Asmo Korkiatupa <asmo.korkiatupa@digia.com>
2020-09-25 16:29:26 +0300

    Test CI pipeline to azure

--- src/AppSettings.ts
+++ src/AppSettings.ts
@@ -1,7 +1,6 @@
 let server: string;
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
-  // dev code
   server = "https://localhost:44330";
 } else {
   server = "https://qandabackend20200925.azurewebsites.net";
